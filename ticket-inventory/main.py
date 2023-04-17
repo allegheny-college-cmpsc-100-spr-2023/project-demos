6efd4d30265490e9093889e5c58d15cdb4a6a1ad
dluman <dluman@allegheny.edu>
2023-04-17 18:35:39 -0400

    Adding comments

--- ticket-inventory/main.py
+++ ticket-inventory/main.py
@@ -1,9 +1,19 @@
+"""
+    Imports Acquire and Factory -- two components of the inventory which:
+    * Acquire: automatically "picks up" and adds item to inventory
+    * Factory: creates new items from either default or designated template
+"""
 from inventory import Acquire, Factory
 
 def main():
+    """ Main method """
+    # Generate 5
     for _ in range(5):
+        # "Factory" a ticket according to template in this repo
         Factory("Ticket", template = "templates/Ticket.py")
+        # "Pick up" the "factoried" ticket
         Acquire("Ticket.py")
 
 if __name__ == "__main__":
+    # Run program beginning at main method
     main()
--- ticket-inventory/templates/Ticket.py
+++ ticket-inventory/templates/Ticket.py
@@ -1,8 +1,12 @@
 class Ticket(ItemSpec):
 
     def __init__(self):
+        """ Constructor """
+        # term-world standard for inheriting ItemSpec
         super().__init__(__file__)
+        # Overriding the inventory system volume, set to 0
         self.VOLUME = 0
     
     def use(self) -> None:
+        """ Functionality executed when used """
         print("USED 1 TICKET")
