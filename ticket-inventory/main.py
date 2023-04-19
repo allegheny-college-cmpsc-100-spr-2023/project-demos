619339f80a61a91bce9becd4d730f331ece22f73
dluman <dluman@allegheny.edu>
2023-04-19 10:25:05 -0400

    Updating with spending example

--- ticket-inventory/main.py
+++ ticket-inventory/main.py
@@ -3,16 +3,35 @@
     * Acquire: automatically "picks up" and adds item to inventory
     * Factory: creates new items from either default or designated template
 """
-from inventory import Acquire, Factory
+from inventory import Acquire, Factory, items
 
-def main():
-    """ Main method """
+def award(amount: int = 0) -> None:
+    """ Reward user with n tickets """
     # Generate 5
-    for _ in range(5):
+    for _ in range(amount):
         # "Factory" a ticket according to template in this repo
         Factory("Ticket", template = "templates/Ticket.py")
         # "Pick up" the "factoried" ticket
         Acquire("Ticket.py")
+
+def spend(cost: int = 0) -> bool:
+    tickets = items.list["Ticket"]["quantity"]
+    if tickets < cost:
+        return False
+    return True
+
+def main():
+    """ Main method """
+    print("""
+1. Get tickets
+2. Spend tickets
+""")
+    choice = int(input("Choose: "))
+    if choice == 1:
+        award(5)
+    else:
+        price = int(input("Cost: "))
+        print(f"Can spend tickets: {spend(price)}")
 
 if __name__ == "__main__":
     # Run program beginning at main method
