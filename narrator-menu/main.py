5beb20ff4d8b5924276a173ab82efff253eedc00
dluman <dluman@allegheny.edu>
2023-04-19 14:41:55 -0400

    Updating with YesNoQuestion

--- narrator-menu/main.py
+++ narrator-menu/main.py
@@ -1,18 +1,26 @@
 import narrator
 
 def main():
-    selection = narrator.Question({
-        "question": "\nChoose a thing:\n",
-        "responses": [
-            {"choice": "1 Soda ", "outcome": 1},
-            {"choice": "2 Beet Juice ", "outcome": 2},
-            {"choice": "3 Super Greens ", "outcome": 3},
-            {"choice": "4 Carrot Juice ", "outcome": 4},
-            {"choice": "5 Apple Juice", "outcome": 5}
-        ]
+    use = narrator.YesNoQuestion({
+        "question": "Use the machine? ",
+        "outcomes": [True, False]
     })
-    response = selection.ask()
-    print(response)
+    
+    response = use.ask()
+
+    if response:
+        selection = narrator.Question({
+            "question": "\nChoose a thing:\n",
+            "responses": [
+                {"choice": "1 Soda ", "outcome": 1},
+                {"choice": "2 Beet Juice ", "outcome": 2},
+                {"choice": "3 Super Greens ", "outcome": 3},
+                {"choice": "4 Carrot Juice ", "outcome": 4},
+                {"choice": "5 Apple Juice", "outcome": 5}
+            ]
+        })
+        response = selection.ask()
+        print(response)
 
 if __name__ == "__main__":
     main()
