4fbb49bdf14bcdca1a4099a44ffafe9dfb665902
dluman <dluman@allegheny.edu>
2023-04-19 14:31:37 -0400

    Adding menu

---
+++ narrator-menu/main.py
@@ -0,0 +1,18 @@
+import narrator
+
+def main():
+    selection = narrator.Question({
+        "question": "\nChoose a thing:\n",
+        "responses": [
+            {"choice": "1 Soda ", "outcome": 1},
+            {"choice": "2 Beet Juice ", "outcome": 2},
+            {"choice": "3 Super Greens ", "outcome": 3},
+            {"choice": "4 Carrot Juice ", "outcome": 4},
+            {"choice": "5 Apple Juice", "outcome": 5}
+        ]
+    })
+    response = selection.ask()
+    print(response)
+
+if __name__ == "__main__":
+    main()
