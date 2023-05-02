468de43d25fabb238c161b06af517ba65ce0441e
dluman <dluman@allegheny.edu>
2023-05-02 15:17:07 -0400

    Updating with breakdown example

--- market-demo/main.py
+++ market-demo/main.py
@@ -6,9 +6,12 @@ from inventory import Acquire, Factory, items
 def main():
     stocks = Market()
     symbol = input("Symbol: ")
-    print(stocks.status(symbol).price)
+    cost = stocks.status(symbol).price
     Factory(symbol, template = "templates/Share.py")
     Acquire(f"{symbol}.py")
+    qty = items.list[symbol]["quantity"]
+    value = qty * cost
+    print(f"{symbol} {qty} @ {cost} -> {value}")
 
 if __name__ == "__main__":
     main()
